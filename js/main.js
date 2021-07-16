a8475404aa6f21c71b3f5a94a767c24d8a73cd03
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-16 16:24:40 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Un alert() espone 5 numeri generati casualmente. (Decidete voi se debbano essere tutti diversi)
+Non appena l'utente schiaccia "ok", parte un timer di 30 secondi. (Bonus: visualizzare il timer)
+Al termine dei 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). (Bonus: far inserire i numeri da un form)
+Dopo che sono stati inseriti i 5 numeri, il software mostra in un alert quanti e quali dei numeri da indovinare sono stati individuati. (Bonus: stampare in pagina il risultato, in alternativa all'alert.)
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+* Commentate il codice. */
+
+// 
